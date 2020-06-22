7c63a26a4454f699548540055a09fb0c50eef032
Thomas Muscarello <muscarello.thomas@gmail.com>
2020-06-22 22:55:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,31 +3,31 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array = [,"Raymond","Jenna","Thomas","Kateri"]
+  
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array<< "Michael"
+  
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift("Amanda")
+  
 end
 
 def remove_element_from_end_of_array(array)
-  create_an_array.pop("Michael")
+  
 end
 
 def remove_element_from_start_of_array(array)
-  create_an_array.shift("Amanda")
+  
 end
 
 def retrieve_element_from_index(array, index_number)
-  create_an_array["Thomas"]
+  
 end
 
 def retrieve_first_element_from_array(array)
-  create_an_array[0]
+  
 end
 
 def retrieve_last_element_from_array(array)
